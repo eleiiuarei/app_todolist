db305f01a08e9a0f6ad0d737fc7951da563f9bd9
eleiiuarei <l.garrido.perez@gmail.com>
2018-06-03 18:54:51 +0200

    'create html and css, forms and input can be improved, but it's ok to start with js

---
+++ js/js_todolist.js
@@ -0,0 +1,10 @@
+//Mostrar new task box
+let addTask = document.getElementById("addTask");
+let theTask = document.getElementById("theTask");
+
+addTask.addEventListener('click', function () {theTask.className = "show_add_task"});
+
+
+
+
+//Mostrar tabs
