f89f9c5cc81635876ab61384611bad293ac0db63
eleiiuarei <l.garrido.perez@gmail.com>
2018-06-03 21:26:16 +0200

    add new item box onclick, and 2 alerts on create/cancel. Styles can be improved

--- js/js_todolist.js
+++ js/js_todolist.js
@@ -2,7 +2,24 @@
 let addTask = document.getElementById("addTask");
 let theTask = document.getElementById("theTask");
 
+let createTask = document.getElementById("btn4Create");
+let cancelTask = document.getElementById("btn4Cancel");
+
+let capa1Create = document.getElementById("capa1Create");
+let createYes = document.getElementById("createBtnYes");
+let createNo = document.getElementById("createBtnNo");
+
+let capa1Cancel = document.getElementById("capa1Cancel");
+let cancelYes = document.getElementById("cancelBtnYes");
+let cancelNo = document.getElementById("cancelBtnNo");
+
 addTask.addEventListener('click', function () {theTask.className = "show_add_task"});
+createTask.addEventListener('click', function () {capa1Create.className ="show_alert"});
+createYes.addEventListener('click', function () {capa1Create.className ="hidden_alert"; theTask.className = "hidden_add_task"});
+createNo.addEventListener('click', function () {capa1Create.className ="hidden_alert"});
+cancelTask.addEventListener('click', function () {capa1Cancel.className ="show_alert"});
+cancelYes.addEventListener('click', function () {capa1Cancel.className ="hidden_alert"; theTask.className = "hidden_add_task"});
+cancelNo.addEventListener('click', function () {capa1Cancel.className = "hidden_alert"});
 
 
 
